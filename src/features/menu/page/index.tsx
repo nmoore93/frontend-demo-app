48c00d7919204363de9f1e1e13c9dffe8397693b
Niall Moore <niall.moore1993@gmail.com>
2025-09-17 10:53:34 +0100

    use inputs on menu page

--- src/features/menu/page/index.tsx
+++ src/features/menu/page/index.tsx
@@ -1,4 +1,5 @@
 import { Page } from "../../../common-components/page";
+import { Input } from "../../../component-library/input";
 import { Tile, TileContent, TileHeader } from "../../../component-library/tile";
 
 export const MenuPage = () => {
@@ -7,12 +8,28 @@ export const MenuPage = () => {
       <div className="flex flex-col gap-7 grow shrink basis-auto p-4">
         <Tile size="large">
           <TileHeader canCollapse title="Item Details" />
-          <TileContent>Item Details</TileContent>
+          <TileContent>
+            <div className="flex flex-col gap-2">
+              <div className="m-0.5">
+                <Input label="Name" />
+              </div>
+              <div className="m-0.5">
+                <Input
+                  description="This will be shown as the description on your menu"
+                  label="Description"
+                />
+              </div>
+            </div>
+          </TileContent>
         </Tile>
 
         <Tile size="large">
           <TileHeader canCollapse title="Item Pricing" />
-          <TileContent>Item Pricing</TileContent>
+          <TileContent>
+            <div className="m-0.5">
+              <Input label="Price" placeholder="$0.00" />
+            </div>
+          </TileContent>
         </Tile>
       </div>
     </Page>
